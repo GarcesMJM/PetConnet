04657fb295c8e9383686dfe90eb593823edadb42
JuanJose <juanj.calderon@upb.edu.co>
2023-10-17 22:08:53 -0500

    Instalado Firebase en el front

---
+++ front_PetConnect/src/Firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBHZ6XTuHPmIy1jnv0yHbLpXaiNqkPqQGY",
+  authDomain: "petconnect-67be4.firebaseapp.com",
+  projectId: "petconnect-67be4",
+  storageBucket: "petconnect-67be4.appspot.com",
+  messagingSenderId: "508382879611",
+  appId: "1:508382879611:web:d885779614c09bef5ce7de",
+  measurementId: "G-7W6QELPCB0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
