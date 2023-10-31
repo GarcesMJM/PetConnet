f71477a934512e228728d3bfb7c4a9dc4349a010
JuanJose <juanj.calderon@upb.edu.co>
2023-10-31 00:16:48 -0500

    Arreglado iniciarsesionController.js

--- back_PetConnect/controllers/iniciarsesionController.js
+++ back_PetConnect/controllers/iniciarsesionController.js
@@ -1,35 +1,31 @@
-const admin = require('firebase-admin');
-const db = admin.firestore();
-const usersRef = db.collection('usuarios');
-const { auth } = require('firebase-admin');
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 async function iniciarsesion(req, res) {
   try {
-    console.log('Controlador de inicio de sesion llamado');
     const { email, password } = req.body;
 
-    const usernameExists = await usersRef.where('email', '==', email).get();
-    const passwordExists = await usersRef.where('password', '==', password).get();
+    // Inicia sesión con correo electrónico y contraseña
+    await signInWithEmailAndPassword(auth, email, password);
 
-    if ( !usernameExists.empty && !passwordExists.empty){
-      console.log("inició sesión")
-    }
-    console.log('hola');
-
-    res.status(201).send({ message: 'Ingresó exitosamente' });
+    return res.status(200).send('Sesión iniciada con éxito');
   } catch (error) {
-    console.error(error);
-
-    // Puedes manejar errores específicos aquí, por ejemplo, verificar si el error es debido a credenciales inválidas
-    if (error.code === 'auth/wrong-password') {
-      return res.status(400).send({ message: 'Contraseña incorrecta' });
-    } else if (error.code === 'auth/user-not-found') {
-      return res.status(404).send({ message: 'Usuario no encontrado' });
-    }
-
-    res.status(500).send({ message: 'Error al iniciar sesión' });
+    console.log('Error al iniciar sesión:', error);
+    return res.status(500).send('Error al iniciar sesión');
   }
 }
-
 module.exports = iniciarsesion;
-
