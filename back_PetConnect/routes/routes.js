732dd8f0b959d4b00088585b3f695a16c4931ed3
JuanJose <juanj.calderon@upb.edu.co>
2023-11-06 00:07:30 -0500

    Agregado controlador para obtener usuario

--- back_PetConnect/routes/routes.js
+++ back_PetConnect/routes/routes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const router = express.Router();
 const registroController = require('../controllers/registroController');
 const iniciarsesionController = require('../controllers/iniciarsesionController');
+const obtenerUsuarioController = require('../controllers/obtenerUsuarioController');
 
 // Rutas de autenticación
 router.post('/register', registroController);
 
 router.post('/iniciarsesion', iniciarsesionController);
+
+router.post('/obtenerusuario', obtenerUsuarioController);
 
 
 module.exports = router;
