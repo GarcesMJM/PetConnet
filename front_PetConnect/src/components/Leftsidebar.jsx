57638d2b3a848c4ea8e3a330cde5bc90f1e87250
Camilo Guzmán <89235206+axensz@users.noreply.github.com>
2023-11-14 22:28:16 -0500

    Posts

--- front_PetConnect/src/components/Leftsidebar.jsx
+++ front_PetConnect/src/components/Leftsidebar.jsx
@@ -6,26 +6,42 @@ export default function Leftsidebar() {
       <div className="col-md-3">
         <div className="card">
           <div className="card-body">
-            <div className="h5">@LeeCross</div>
-            <div className="h7 text-muted">Fullname : Miracles Lee Cross</div>
+            <div className="h5">Explora nuestras Publicaciones</div>
             <div className="h7">
-              Developer of web applications, JavaScript, PHP, Java, Python,
-              Ruby, Java, Node.js, etc.
+              Descubre historias conmovedoras de adopciones exitosas, consejos esenciales para el cuidado de mascotas y las últimas noticias sobre protección animal.            </div>
+          </div>
+          <div className="card">
+            <div className="card-body">
+              <div className="h5">Alertas de Mascotas Perdidas</div>
+              <div className="h7">
+              Contribuye a reunir familias perdidas. Mantente informado sobre alertas de mascotas extraviadas y únete a la búsqueda para asegurar su regreso a casa.</div>
             </div>
           </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">
-              <div className="h6 text-muted">Followers</div>
-              <div className="h5">5.2342</div>
-            </li>
-            <li className="list-group-item">
-              <div className="h6 text-muted">Following</div>
-              <div className="h5">6758</div>
-            </li>
-            <li className="list-group-item">Vestibulum at eros</li>
-          </ul>
+          <div className="card">
+            <div className="card-body">
+              <div className="h5">Comentarios Conmovedores</div>
+              <div className="h7">
+              Lee los Comentarios inspiradores de adoptantes felices y descubre cómo la adopción ha transformado la vida de estas mascotas y sus dueños.</div>
+            </div>
+          </div>
+        </div>
+
+      <div className="card mt-4">
+        <div className="card-body">
+          <div className="h6 text-muted">Usuarios Totales</div>
+          <div className="h5">Número</div>
         </div>
       </div>
+
+      <div className="card">
+        <div className="card-body">
+          <div className="h6 text-muted">Mascotas Totales</div>
+          <div className="h5">Número</div>
+        </div>
+      </div>
+
+      </div>
+
     </>
   );
 }
